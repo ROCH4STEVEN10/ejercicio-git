fe97aac853a0a05ec1ddc6d5d1d6e4fe6c39189d
BRIAN ROCHA <bsrocha80@soy.sena.edu.co>
2025-02-12 21:48:20 -0500

    commit2BrianRocha

---
+++ algo.php
@@ -0,0 +1,7 @@
+<?php
+    echo "Hola mundo!";
+?>
+<?php
+date_default_timezone_set('America/New_York'); // Ajusta la zona horaria según necesites
+echo "La fecha y hora actual es: " . date("Y-m-d H:i:s");
+?>
